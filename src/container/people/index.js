911cb934b5cd8991377dc2b8cdb33350f28f8e42
lh0412 <3123384778@qq.com>
2021-12-15 17:01:06 +0800

    222

--- src/container/people/index.js
+++ src/container/people/index.js
@@ -1,5 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
+import './people.less';
 function People(props) {
-  return <div>people</div>
+  const [tabList, setTab] = useState([{ name: '全部', code: 0 }, { name: '待支付', code: 1 }, { name: '待收货', code: 2 }])
+  const [curTab, setCurTab] = useState(0);
+  return <div className="people-wrap">
+    <ul className="tab-wrap">
+      {tabList && tabList.map((item, index) => (
+        <li key={index} className={curTab == index ? 'actved' : 'unactive'}>{item.name}</li>
+      ))}
+    </ul>
+    <div className=''>
+
+    </div>
+  </div>
 }
 export default People;
